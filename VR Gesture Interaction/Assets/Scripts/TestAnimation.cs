41aca1f80781be09073d53ed9532cea869727173
Jack-Pettigrew <36480371+Jack-Pettigrew@users.noreply.github.com>
2019-01-22 13:47:21 +0000

    NPC Animation Integration
    Testing NPC animation

---
+++ VR Gesture Interaction/Assets/Scripts/TestAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimation : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Animator>().SetTrigger("Dance");
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Animator>().SetTrigger("Wave");
+        }
+
+
+    }
+}
