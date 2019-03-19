4942c01a3bcf743a1640bc9915629917b29ebbb6
Jack Pettigrew <36480371+Jack-Pettigrew@users.noreply.github.com>
2019-03-19 19:22:30 +0000

    Started Dialogue system

---
+++ VR Gesture Interaction/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public enum Response { neutral, positive, negative }
+
+[System.Serializable]
+public struct Dialogue
+{
+    [TextArea]
+    public string dialogue;
+    public AnimationClip animation;
+    public Response responseType;
+    public Vector3 padEffect;
+    public bool finished;
+
+}
+
+public class DialogueManager : MonoBehaviour
+{
+
+    [Header("Dialogue Properties"), SerializeField]
+    private bool readyToStart;
+
+    [Header("NPC Dialogue")]
+    public Dialogue[] npcDialogues;
+
+
+
+    // With each dialogue, change PAD interact enum
+
+    // Read dialogue
+    // Wait for response (dialogue tree / gesture)
+    // Get next dialogue
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+}
