f9d28d7253ad70d90c8017df3ef1e162b5f0355e
Jack Pettigrew <36480371+Jack-Pettigrew@users.noreply.github.com>
2019-04-01 21:04:13 +0100

    Added Nav Mesh

---
+++ VR Gesture Interaction/Assets/Scripts/NpcNav.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcNav : MonoBehaviour
+{
+    private Animator ani;
+    private NavMeshAgent nma;
+    public Transform[] npcWalkPoints;
+    public const float TIMER_CONST = 5.0f;
+    public float pointTimer;
+
+    private Transform nextPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nma = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+
+        pointTimer = TIMER_CONST;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position == nma.pathEndPosition)
+        {
+            pointTimer -= Time.deltaTime;
+
+            if(pointTimer <= 0)
+            {
+                pointTimer = TIMER_CONST;
+                NextPoint();
+            }
+        }
+    }
+
+    void NextPoint()
+    {
+        nextPoint = npcWalkPoints[Random.Range(0, npcWalkPoints.Length)];
+
+        nma.SetDestination(nextPoint.position);
+    }
+}
