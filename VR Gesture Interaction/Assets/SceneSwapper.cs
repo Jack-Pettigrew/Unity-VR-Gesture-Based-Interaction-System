b72aa23cea3a84c01cc2798f27e66b45920b2a56
Jack Pettigrew <36480371+Jack-Pettigrew@users.noreply.github.com>
2020-03-05 17:46:54 +0000

    Fixed OVR Positioning + Added Scene Swapper & Quest Controllers

---
+++ VR Gesture Interaction/Assets/SceneSwapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwapper : MonoBehaviour
+{
+    public int sceneIndex = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.RawButton.B))
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
